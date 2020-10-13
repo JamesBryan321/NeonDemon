34418fd4ce6a92b4819f22cffc5e64dc84747347
PapaJepo <44029409+PapaJepo@users.noreply.github.com>
2020-10-13 17:47:52 +0100

    Added UI for gun and fixed reload

--- NeonDemonProject/Assets/Scenes/ConnorFolder/Shooting.cs
+++ NeonDemonProject/Assets/Scenes/ConnorFolder/Shooting.cs
@@ -5,23 +5,26 @@ using TMPro;
 
 public class Shooting : MonoBehaviour
 {
+    public TMP_Text AmmoCount;
+    public float Ammo = 12;
     public Camera cam;
     public List<ParticleSystem> ShootingSFX;
     public GameObject impactEffect;
-    Animator Gun_Anim;
+    public Animator Gun_Anim;
     // Start is called before the first frame update
     void Start()
     {
-        Gun_Anim = GetComponent<Animator>();
+       // Gun_Anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        AmmoCount.text = "" + Ammo;
         //////
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && Ammo > 0)
         {
+            Ammo -= 1;
             int randomNum = Random.Range(0, 2);
             Shoot();
             ShootingSFX[randomNum].Emit(1);
@@ -29,6 +32,7 @@ public class Shooting : MonoBehaviour
         else if(Input.GetMouseButtonDown(1))
         {
             Gun_Anim.SetTrigger("Reload");
+            Ammo = 12;
         }
     }
 
@@ -40,6 +44,7 @@ public class Shooting : MonoBehaviour
         {
             GameObject impactEffectGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(Vector3.forward, Vector3.up)) as GameObject;
             Destroy(impactEffectGO, 5);
+           
             Debug.Log( hit.collider.gameObject.name);
         }
     }
