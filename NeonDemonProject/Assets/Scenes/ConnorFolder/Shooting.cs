1dfd7b1b2423dcf958acbe4fb7e8afa11e647874
PapaJepo <44029409+PapaJepo@users.noreply.github.com>
2020-10-13 16:37:45 +0100

    Added particle effect package

--- NeonDemonProject/Assets/Scenes/ConnorFolder/Shooting.cs
+++ NeonDemonProject/Assets/Scenes/ConnorFolder/Shooting.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class Shooting : MonoBehaviour
 {
+    public Camera cam;
     public List<ParticleSystem> ShootingSFX;
+    public GameObject impactEffect;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,10 +17,25 @@ public class Shooting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+
+        //////
+        if (Input.GetMouseButtonDown(0))
         {
             int randomNum = Random.Range(0, 2);
+            Shoot();
             ShootingSFX[randomNum].Emit(1);
+        }
+    }
+
+    void Shoot()
+    {
+        Ray ray = cam.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit,Mathf.Infinity))
+        {
+            GameObject impactEffectGO = Instantiate(impactEffect, hit.point, Quaternion.identity) as GameObject;
+            Destroy(impactEffectGO, 5);
+            Debug.Log( hit.collider.gameObject.name);
         }
     }
 }
