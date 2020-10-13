efb0a37c5807a70d02e7dd944527bb935e9fec06
PapaJepo <44029409+PapaJepo@users.noreply.github.com>
2020-10-13 16:09:16 +0100

    Sfx for shooting

---
+++ NeonDemonProject/Assets/Scenes/ConnorFolder/Shooting.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public List<ParticleSystem> ShootingSFX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            int randomNum = Random.Range(0, 2);
+            ShootingSFX[randomNum].Emit(1);
+        }
+    }
+}
