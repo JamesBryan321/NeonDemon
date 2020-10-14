26f4a44f9e11f2a127e04972aaa4eba945744295
Dazariath <c17366881@mydit.ie>
2020-10-14 16:52:33 +0100

    Added player health UI

---
+++ NeonDemonProject/Assets/Scenes/DarraghFolder/Healthbar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Healthbar : MonoBehaviour
+{
+
+    public Slider slider;
+
+    public float currentHealth = 0.5f;
+
+    public float maxHealth = 1f;
+
+
+    public void SetMaxHealth()
+    {
+        slider.maxValue = maxHealth;
+    }
+
+    public void Update()
+    {
+        slider.value = currentHealth;
+    }
+}
