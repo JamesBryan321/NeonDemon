74c901159cf784e1843b7a32bf257657cf0e5c67
James Bryan <jamesbryangamess@gmail.com>
2020-11-03 01:37:11 +0000

    player arm (meta rig) for ainmations implmented

---
+++ NeonDemonProject/Assets/Destructable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructable : MonoBehaviour
+{
+    public GameObject Destroyedversion;
+    public GameObject Canvas;
+    public GameObject scorePrefab;
+    public GameObject comboScore;
+    public AudioSource audioData;
+
+
+
+    private void Start()
+    {
+        audioData = GetComponent<AudioSource>();
+
+        Break();
+
+    }
+
+    public void Break()
+    {
+        Instantiate(Destroyedversion, transform.position, transform.rotation);
+        Destroy(gameObject);
+
+        audioData.Play(0);
+
+    }
+}
