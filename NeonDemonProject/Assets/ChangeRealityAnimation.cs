a209b9b7ef33417288212773dd64abb5cbf93f25
James Bryan <jamesbryangamess@gmail.com>
2020-11-13 14:39:56 +0000

    reality change has more feedback

---
+++ NeonDemonProject/Assets/ChangeRealityAnimation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeRealityAnimation : MonoBehaviour
+{
+
+    public Animator Belial_Anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+        
+            Belial_Anim.SetBool("RealityChange", true);
+            StartCoroutine(WaitForChange());
+        }
+
+    }
+
+    public IEnumerator WaitForChange()
+    {
+        yield return new WaitForSeconds(1);
+
+        Belial_Anim.SetBool("RealityChange", false);
+    }
+}
