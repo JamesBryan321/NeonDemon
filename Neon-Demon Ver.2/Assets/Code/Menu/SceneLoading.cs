1300aed4d6795e5efaa61c8061abdba8e68f1b72
PapaJepo <44029409+PapaJepo@users.noreply.github.com>
2020-12-15 19:19:09 +0000

    Main Menu Scene Loading

---
+++ Neon-Demon Ver.2/Assets/Code/Menu/SceneLoading.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoading : MonoBehaviour
+{
+    [Header("CameraTransition")]
+    public Animator Cam;
+    [SerializeField] private GameObject Fade;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+   public void Transistion()
+    {
+        Cam.SetTrigger("start");
+    }
+
+    public void StartFade()
+    {
+        Fade.SetActive(true);
+    }
+
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
