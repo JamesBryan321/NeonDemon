012ac6a899f927eaf737d08ee4f35c34047ca62a
PapaJepo <44029409+PapaJepo@users.noreply.github.com>
2020-12-15 19:49:09 +0000

    Fixed reality script

--- Neon-Demon Ver.2/Assets/Code/Player/RealityChange.cs
+++ Neon-Demon Ver.2/Assets/Code/Player/RealityChange.cs
@@ -14,7 +14,7 @@ public class RealityChange : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-    //    realityNormal = true;
+      realityNormal = true;
     }
 
     // Update is called once per frame
