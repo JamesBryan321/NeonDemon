33a4b8f1da30485e606e614b044f9b7a8643340e
SlimShaney <44029427+SlimShaney@users.noreply.github.com>
2020-12-18 22:14:11 +0000

    EndZone set up in arena

---
+++ Neon-Demon Ver.2/Assets/Code/FinishArena.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishArena : MonoBehaviour
+{
+    [SerializeField] public SceneChange sceneScript;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        sceneScript.LoadMainMenu();
+    }
+}
