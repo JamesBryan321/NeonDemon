df3465ebe2cda01563d47453d2e70f3f8989436e
Dazariath <c17366881@mydit.ie>
2020-12-19 15:09:35 +0000

    Added Cutscene

---
+++ Neon-Demon Ver.2/Assets/Timeline/StartLevelCutscene.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartLevelCutscene : MonoBehaviour
+{
+    public GameObject CutsceneCamera;
+    public GameObject Timeline;
+
+    public GameObject Trigger;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CutsceneCamera.SetActive(false);
+        Timeline.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider Player)
+    {
+        CutsceneCamera.SetActive(true);
+        Timeline.SetActive(true);
+    }
+
+    private void OnTriggerExit(Collider Player)
+    {
+        CutsceneCamera.SetActive(false);
+        Timeline.SetActive(false);
+
+        Trigger.SetActive(false);
+    }
+}
