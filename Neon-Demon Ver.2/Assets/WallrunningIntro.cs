1a728297f78b709efe4a423395a1e888c9aa0924
Dazariath <c17366881@mydit.ie>
2020-12-19 22:42:33 +0000

    added hell walls, dumbo arrows and wallrunning intro text

---
+++ Neon-Demon Ver.2/Assets/WallrunningIntro.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallrunningIntro : MonoBehaviour
+{
+    public GameObject BelialText;
+    public GameObject Trigger;
+
+    private void Start()
+    {
+        BelialText.SetActive(false);
+
+        Trigger.SetActive(true);
+    }
+
+    private void OnTriggerEnter(Collider Player)
+    {
+        BelialText.SetActive(true);
+    }
+
+    private void OnTriggerExit(Collider Player)
+    {
+        BelialText.SetActive(false);
+
+        Trigger.SetActive(false);
+    }
+}
