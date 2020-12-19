d8feba7d8ecab35693b775ff8d12ea0c74ce397b
Dazariath <c17366881@mydit.ie>
2020-12-19 15:28:26 +0000

    added scene changer to tutorial

---
+++ Neon-Demon Ver.2/Assets/Code/FinishIntroComic.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishIntroComic : MonoBehaviour
+{
+    [SerializeField] public SceneChange sceneScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Function());
+    }
+    IEnumerator Function()
+    {
+        yield return new WaitForSeconds(70);
+
+        sceneScript.LoadTutorial();
+    }
+}
