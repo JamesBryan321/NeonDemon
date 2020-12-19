09667ab7853ac9173d10be63660e87be04d3943d
Dazariath <c17366881@mydit.ie>
2020-12-19 17:17:02 +0000

    Added tutorial text to the main level for reality blink and slowdown

---
+++ Neon-Demon Ver.2/Assets/realityBlinkIntro.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class realityBlinkIntro : MonoBehaviour
+{
+    public GameObject BelialText;
+    //public GameObject Trigger_RealityBlinkIntro;
+
+    private void OnTriggerEnter(Collider Player)
+    {
+        BelialText.SetActive(true);
+    }
+
+    private void OnTriggerExit(Collider Player)
+    {
+        BelialText.SetActive(false);
+        //Trigger_RealityBlinkIntro.SetActive(false);
+    }
+}
