773127ec9a3e89a8a5284f849b2772c3ba7c933a
SlimShaney <44029427+SlimShaney@users.noreply.github.com>
2020-12-19 14:24:31 +0000

    SliderScript added

---
+++ Neon-Demon Ver.2/Assets/Code/SettingsSliders.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsSliders : MonoBehaviour
+{
+    public Slider brightnessSlider;
+
+    public float defaultBrightnessValue = 1;
+    void Start()
+    {
+        brightnessSlider.value = defaultBrightnessValue;
+    }
+
+    private void OnGUI()
+    {
+        //defaultBrightnessValue = brightnessSlider();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RenderSettings.ambientIntensity = brightnessSlider.value;
+    }
+}
