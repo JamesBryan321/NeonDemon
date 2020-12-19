9d4434f1e68dda382a56fab6892dab2f8fe8fc31
PapaJepo <44029409+PapaJepo@users.noreply.github.com>
2020-12-19 13:40:45 +0000

    Health Pickup Finished

---
+++ Neon-Demon Ver.2/Assets/Code/Player/HealthScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthScript : MonoBehaviour
+{
+    public PlayerHP HealthRef;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            if(HealthRef.PlayerHealth < 100)
+            {
+                HealthRef.PlayerHealth += 50;
+                Destroy(gameObject);
+
+            }
+        }
+    }
+}
