a1a89e25b62005f1281a8460e27cca7c97e89934
PapaJepo <44029409+PapaJepo@users.noreply.github.com>
2021-02-11 15:37:22 +0000

    SlowDownTest

--- Neon-Demon Ver.2/Assets/Code/Player/SlowDownTime.cs
+++ Neon-Demon Ver.2/Assets/Code/Player/SlowDownTime.cs
@@ -7,7 +7,7 @@ public class SlowDownTime : MonoBehaviour
 {
     public GameObject Meter;
     public int MeterAmount = 100;
-
+    public bool devtest;
     private int TimeStop = 1;
     // Start is called before the first frame update
     void Start()
@@ -26,15 +26,30 @@ public class SlowDownTime : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Meter.GetComponent<Image>().fillAmount > 0 && TimeStop < 0)
+        if (devtest == false)
         {
-            Meter.GetComponent<Image>().fillAmount -= 0.005f;
-            //TimeRef.DoSlowmotion();
-            Time.timeScale = 0.5f;
+            if (Meter.GetComponent<Image>().fillAmount > 0 && TimeStop < 0)
+            {
+                Meter.GetComponent<Image>().fillAmount -= 0.005f;
+                //TimeRef.DoSlowmotion();
+                Time.timeScale = 0.5f;
+            }
+            else
+            {
+                Time.timeScale = 1f;
+            }
         }
         else
         {
-            Time.timeScale = 1f;
+            if (TimeStop < 0)
+            {
+               
+                Time.timeScale = 0.5f;
+            }
+            else
+            {
+                Time.timeScale = 1f;
+            }
         }
     }
 
