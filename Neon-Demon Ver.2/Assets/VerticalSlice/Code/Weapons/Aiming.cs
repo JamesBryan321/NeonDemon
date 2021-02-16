0470fec43164fa5911cc8a900a045d217c00eaeb
SlimShaney <44029427+SlimShaney@users.noreply.github.com>
2021-02-16 15:04:36 +0000

    Sensitivity setting for mouse or console

--- Neon-Demon Ver.2/Assets/VerticalSlice/Code/Weapons/Aiming.cs
+++ Neon-Demon Ver.2/Assets/VerticalSlice/Code/Weapons/Aiming.cs
@@ -5,15 +5,30 @@ using UnityEngine;
 public class Aiming : MonoBehaviour
 {
     public Transform playerBody;
-    public float Sensitivity = 100f;
+    private float sensitivity = 100f;
     public float xRot = 0f;
     public float yRot = 0f;
 
     private float cameraRotation;
     private float cameraTilt;
+
+    public InputController inputScript;
+    public float controllerSensitivity;
+    public float mouseSensitivity;
+    
     void Start()
     {
         Cursor.visible = false;
+        if (inputScript.useController)
+        {
+            sensitivity = controllerSensitivity;
+        }
+
+        if (inputScript.useKeyboard)
+        {
+            sensitivity = mouseSensitivity;
+        }
+        //Debug.Log("Camera sensitivity is set to: " + sensitivity);
     }
 
     public void OnRotateInput(float cameraRotation, float cameraTilt)
@@ -25,10 +40,10 @@ public class Aiming : MonoBehaviour
 
     void Update()
     {
-        xRot -= cameraTilt * Sensitivity * Time.deltaTime;
+        xRot -= cameraTilt * sensitivity * Time.deltaTime;
         xRot = Mathf.Clamp(xRot, -90f, 90f);
 
-        yRot += cameraRotation * Sensitivity * Time.deltaTime;
+        yRot += cameraRotation * sensitivity * Time.deltaTime;
         transform.localRotation = Quaternion.Euler(xRot, 0f, 0f);
         playerBody.Rotate(Vector3.up * cameraRotation);
         playerBody.localRotation = Quaternion.Euler(0f, yRot, 0f);
