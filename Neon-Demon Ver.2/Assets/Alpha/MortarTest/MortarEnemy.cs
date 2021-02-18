6837dd02b36d421d756af0701e4eb90aceec7b8e
PapaJepo <44029409+PapaJepo@users.noreply.github.com>
2021-02-18 15:19:34 +0000

    Mortar Basics

---
+++ Neon-Demon Ver.2/Assets/Alpha/MortarTest/MortarEnemy.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MortarEnemy : MonoBehaviour
+{
+    public GameObject Player;
+    public LineRenderer linerenderer;
+    public Transform point0, point1, point2;
+    private int numPoints = 300;
+    public Vector3[] positions = new Vector3[300];
+    public GameObject Mortar;
+    public Transform firepoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        linerenderer.positionCount = numPoints;
+        DrawLinearCurve();
+    }
+
+    public void TestMortar()
+    {
+        Debug.Log("Shoot");
+        Instantiate(Mortar, firepoint);
+    }
+
+
+        // Update is called once per frame
+        void Update()
+    {
+        DrawLinearCurve();
+        point1.position = new Vector3((point0.position.x + point2.position.x) / 2 , ((point0.position.y + point2.position.y) / 2) + 10, ((point0.position.z + point2.position.z) / 2) );
+        point2.position = Player.transform.position;
+
+
+    }
+
+    private void DrawLinearCurve()
+    {
+        for (int i = 1; i < numPoints + 1; i++)
+        {
+            float t = i / (float)numPoints;
+            positions[i - 1] = CalculateQuadraticBezierPoint(t, point0.position, point1.position, point2.position);
+        }
+        linerenderer.SetPositions(positions);
+    }
+
+    #region Bezier Maths
+    private Vector3 CalculateLinerBezierPoint(float t, Vector3 p0, Vector3 p1)
+    {
+        return p0 + t * (p1 - p0);
+    }
+
+    private Vector3 CalculateQuadraticBezierPoint(float t, Vector3 p0, Vector3 p1, Vector3 p2)
+    {
+        float u = 1 - t;
+        float tt = t * t;
+        float uu = u * u;
+        Vector3 p = uu * p0;
+        p += 2 * u * t * p1;
+        p += tt * p2;
+        return p;
+    }
+    #endregion
+}
---
+++ Neon-Demon Ver.2/Assets/Alpha/MortarTest/MortarProjectile.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MortarProjectile : MonoBehaviour
+{
+    public int CurrentWaypointID = 0;
+    public float speed;
+    private float reachDistance = 1f;
+    private MortarEnemy LineRef;
+    Vector3 last_position;
+    Vector3 current_position;
+    //public GameObject LineRef;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LineRef = GameObject.Find("MortarEnemy").GetComponent<MortarEnemy>();
+        transform.position = LineRef.positions[CurrentWaypointID];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position = LineRef.positions[CurrentWaypointID];
+        //Debug.Log(CurrentWaypointID);
+        if (CurrentWaypointID < 299)
+        {
+            Debug.Log("TEst");
+            transform.position = LineRef.positions[CurrentWaypointID];
+            if (Vector3.Distance(LineRef.positions[CurrentWaypointID], this.transform.position) < 1)
+            {
+                CurrentWaypointID = (CurrentWaypointID + 1);//% LineRef.positions.Length+1;
+            }
+        }
+        else if (CurrentWaypointID >= 299)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
