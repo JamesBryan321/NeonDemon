c452c289deea0e2921b693e653685a18d9698af4
PapaJepo <44029409+PapaJepo@users.noreply.github.com>
2021-02-22 17:08:28 +0000

    Code Commit

--- Neon-Demon Ver.2/Assets/Alpha/MortarTest/MortarEnemy.cs
+++ Neon-Demon Ver.2/Assets/Alpha/MortarTest/MortarEnemy.cs
@@ -13,7 +13,7 @@ public class MortarEnemy : MonoBehaviour
     public GameObject Mortar;
     public GameObject AimLocation;
     public Transform firepoint;
-
+    public float detection = 70;
     public float Firerate = 1f;
     public float nextFire = 0f;
     // Start is called before the first frame update
@@ -40,16 +40,19 @@ public class MortarEnemy : MonoBehaviour
         void Update()
     {
         DrawLinearCurve();
-       // point1.position = new Vector3((point0.position.x + point2.position.x) / 2 , ((point0.position.y + point2.position.y) / 2) + 10, ((point0.position.z + point2.position.z) / 2) );
-       // point2.position = Player.transform.position;
-       if(Time.time > nextFire)
-        {
-            nextFire = Time.time + Firerate;
-            GameObject mortar = Instantiate(Mortar, firepoint);
-            mortar.GetComponent<MortarProjectile>().Followpositions = positions;
-            point1.position = new Vector3((point0.position.x + point2.position.x) / 2, ((point0.position.y + point2.position.y) / 2) + CurveHeight, ((point0.position.z + point2.position.z) / 2));
-            point2.position = Player.transform.position;
-            Instantiate(AimLocation, point2);
+        // point1.position = new Vector3((point0.position.x + point2.position.x) / 2 , ((point0.position.y + point2.position.y) / 2) + 10, ((point0.position.z + point2.position.z) / 2) );
+        // point2.position = Player.transform.position
+        if (Vector3.Distance(Player.transform.position, this.transform.position) < detection)
+             {
+            if (Time.time > nextFire)
+            {
+                nextFire = Time.time + Firerate;
+                GameObject mortar = Instantiate(Mortar, firepoint);
+                mortar.GetComponent<MortarProjectile>().Followpositions = positions;
+                point1.position = new Vector3((point0.position.x + point2.position.x) / 2, ((point0.position.y + point2.position.y) / 2) + CurveHeight, ((point0.position.z + point2.position.z) / 2));
+                point2.position = Player.transform.position;
+                Instantiate(AimLocation, point2);
+            }
         }
 
     }
