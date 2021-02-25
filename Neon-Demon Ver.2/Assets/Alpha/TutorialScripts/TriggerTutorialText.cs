d8209641ce0fb01bda38e6de8ab7583319f0c81b
Dazariath <c17366881@mydit.ie>
2021-02-25 16:49:32 +0000

    added dialogue text boxes

---
+++ Neon-Demon Ver.2/Assets/Alpha/TutorialScripts/TriggerTutorialText.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerTutorialText : MonoBehaviour
+{
+    public GameObject BelialText;
+
+    private void Start()
+    {
+        BelialText.SetActive(false);
+
+        this.gameObject.SetActive(true);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            BelialText.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            BelialText.SetActive(false);
+
+            this.gameObject.SetActive(false);
+        }
+
+    }
+}
