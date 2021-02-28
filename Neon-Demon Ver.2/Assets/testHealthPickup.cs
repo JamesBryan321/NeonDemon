ff09bff2dcc1534117f30d0c4529852c8d833411
PapaJepo <44029409+PapaJepo@users.noreply.github.com>
2021-02-28 17:14:09 +0000

    level changes

---
+++ Neon-Demon Ver.2/Assets/testHealthPickup.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testHealthPickup : MonoBehaviour
+{
+    public PlayerHP HealthRef;
+    public float Firerate;
+    public float nextFire = 0f;
+    private bool checktime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        if (Time.time > nextFire)
+        {
+            gameObject.SetActive(true);
+            checktime = false;
+        }
+
+        /*
+        if(checktime == true)
+        {
+            nextFire = Time.time + Firerate;
+        }
+        */
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            //checktime = true;
+            StartCoroutine(Wait());
+            HealthRef.PlayerHealth += 1f;
+            gameObject.SetActive(false);
+
+        }
+    }
+
+    IEnumerator Wait()
+    {
+        gameObject.SetActive(true);
+        yield return new WaitForSeconds(2f);
+    }
+
+}
