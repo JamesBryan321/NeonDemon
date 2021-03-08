e94ea29a998c7f42e14776d9a9ab751efb35222e
PapaJepo <44029409+PapaJepo@users.noreply.github.com>
2021-03-08 17:24:29 +0000

    boss stuff

---
+++ Neon-Demon Ver.2/Assets/Boss/AudioPeer.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPeer : MonoBehaviour
+{
+    public AudioSource _audioSource;
+    public static float[] _samples = new float[512];
+    public static float[] _freqBand = new float[8];
+    public static float[] _bandBuffer = new float[8];
+    float[] _bufferDecrease = new float[8];
+    // Start is called before the first frame update
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetSpectrumAudioSource();
+        MakeFrequencyBands();
+        BandBuffer();
+    }
+
+    void GetSpectrumAudioSource()
+    {
+        _audioSource.GetSpectrumData(_samples, 0, FFTWindow.Blackman);
+    }
+
+    void BandBuffer()
+    {
+        for (int i = 0; i < 8; ++i)
+        {
+            if(_freqBand[i] > _bandBuffer[i])
+            {
+                _bandBuffer[i] = _freqBand[i];
+                _bufferDecrease[i] = 0.005f;
+            }
+            if (_freqBand[i] < _bandBuffer[i])
+            {
+                _bandBuffer[i] -= _bufferDecrease[i];
+                _bufferDecrease[i] *= 1.2f;
+            }
+        }
+    }
+
+    void MakeFrequencyBands()
+    {
+        int count = 0;
+        for (int i = 0; i < 8; i++)
+        {
+            float average = 0;
+            int samplecount = (int)Mathf.Pow(2, i) * 2;
+            if (i == 7)
+            {
+                samplecount += 2;
+            }
+            for (int j = 0; j < samplecount; j++)
+            {
+                average += _samples[count] * (count + 1);
+                count++;
+            }
+            average /= count;
+            _freqBand[i] = average * 10;
+        }
+
+
+
+    }
+    }
+        
+    
+             
