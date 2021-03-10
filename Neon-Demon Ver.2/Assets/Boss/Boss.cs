1e72df50b6880f4e9e4b5d204f4b30379913b59a
PapaJepo <44029409+PapaJepo@users.noreply.github.com>
2021-03-10 14:47:20 +0000

    boss changes

--- Neon-Demon Ver.2/Assets/Boss/Boss.cs
+++ Neon-Demon Ver.2/Assets/Boss/Boss.cs
@@ -6,7 +6,7 @@ using UnityEngine.AI;
 public class Boss : MonoBehaviour
 {
    
-    private enum BossState {MOVE, ATTACK1, ATTACK2, VUNERABLE}
+    private enum BossState {INVUNERABLE,MOVE, CHARGE, ATTACK1, ATTACK2, VUNERABLE}
     private BossState b_BossState;
 
     [Header("Boss Stats")]
@@ -19,16 +19,19 @@ public class Boss : MonoBehaviour
     [SerializeField] private GameObject Player;
 
     public Transform Attack2Pos;
+    public Transform LastPlayerRef;
+    public Transform InvunerablePos;
 
     public NavMeshAgent b_navMeshAgent;
     UnityEngine.AI.NavMeshAgent agent;
     private IEnumerator coroutine;
-    private IEnumerator Vcoroutine;
+    private IEnumerator Chargecoroutine;
+    private IEnumerator INVUNcoroutine;
 
     private int TempHP;
 
     private Renderer MaterialColour;
-    public Color AttackColour, VunerableColour;
+    public Color AttackColour, VunerableColour,ChargeColour;
     // Start is called before the first frame update
     void Start()
     {
@@ -48,14 +51,20 @@ public class Boss : MonoBehaviour
             case BossState.MOVE:
 
                 break;
+            case BossState.CHARGE:
+                Charge();
+                break;
             case BossState.ATTACK1:
-
+                Attack1();
                 break;
             case BossState.ATTACK2:
                 Attack2();
                 break;
             case BossState.VUNERABLE:
                 Vunerable();
+                break;
+            case BossState.INVUNERABLE:
+                Invunerable();
                 break;
             default:
                 break;
@@ -70,13 +79,45 @@ public class Boss : MonoBehaviour
         BossVunerable = false;
     }
 
+    void Charge()
+    {
+        BossVunerable = false;
+        TempHP = BossHealth;
+        Chargecoroutine = AttackCharge(3.0f);
+        StartCoroutine(Chargecoroutine);
+    }
+
+    IEnumerator AttackCharge(float ChargeTime)
+    {
+        MaterialColour.material.SetColor("_Color", ChargeColour);
+        LastPlayerRef.position = Player.transform.position;
+        yield return new WaitForSeconds(ChargeTime);
+        b_BossState = BossState.ATTACK1;
+    }
+
     void Attack1()
     {
         BossVunerable = false;
+        MaterialColour.material.SetColor("_Color", AttackColour);
+        this.transform.LookAt(Player.transform);
+        agent.speed = 200;
+        agent.acceleration = 70;
+       
+
+        if (Vector3.Distance(LastPlayerRef.position, this.transform.position) < 5)
+        {
+            agent.speed = 0;
+            b_BossState = BossState.VUNERABLE;
+        }
+        else
+        {
+            b_navMeshAgent.SetDestination(LastPlayerRef.position);
+        }
     }
 
     void Attack2()
     {
+        
         BossVunerable = false;
         MaterialColour.material.SetColor("_Color", AttackColour);
         if(Vector3.Distance(Attack2Pos.position, this.transform.position)<3)
@@ -116,14 +157,42 @@ public class Boss : MonoBehaviour
 
         if (BossHealth < TempHP)
         {
-            b_BossState = BossState.ATTACK2;
+            agent.speed = 10;
+            if (BossHealth == 4 || BossHealth == 2)
+            {
+                agent.speed = 10;
+                b_BossState = BossState.INVUNERABLE;
+            }
+            else
+            {
+                b_BossState = BossState.CHARGE;
+
+            }
         }
+
+       
     }
-    /*
-    IEnumerator VunerableCooldown(float waitTime)
+
+    void Invunerable()
     {
-        yield return new WaitForSeconds(waitTime);
+        BossVunerable = false;
+        if (Vector3.Distance(InvunerablePos.position, this.transform.position) > 3)
+        {
+            b_navMeshAgent.SetDestination(InvunerablePos.position);
+        }
+        else
+        {
+           INVUNcoroutine = InvunTime(5.0f);
+           StartCoroutine(INVUNcoroutine);
+
+        }
+        
+    }
+
+    IEnumerator InvunTime(float invuncdr)
+    {
+        yield return new WaitForSeconds(invuncdr);
         b_BossState = BossState.ATTACK2;
     }
-    */
+       
 }
