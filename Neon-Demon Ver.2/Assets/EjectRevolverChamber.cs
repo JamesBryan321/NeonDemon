6feb579725b00b0e456749381885be83f0ad9062
Dazariath <c17366881@mydit.ie>
2021-03-22 18:02:12 +0000

    added boss textures and tried to edit revolver animations

---
+++ Neon-Demon Ver.2/Assets/EjectRevolverChamber.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EjectRevolverChamber : MonoBehaviour
+{
+    private Rigidbody ChamberRB;
+    private GameObject GunChamberLocation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ChamberRB = this.gameObject.GetComponent<Rigidbody>();
+        GunChamberLocation = GameObject.Find("GunChamberLocation");
+
+        ChamberRB.AddForce(this.gameObject.transform.up * 10f);
+    }
+}
---
+++ Neon-Demon Ver.2/Assets/SpawnChamber.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnChamber : MonoBehaviour
+{
+    public GameObject GunChamberPrefab;
+    private Vector3 GunChamberLocation;
+
+    private void Start()
+    {
+    }
+
+    void Spawn()
+    {
+        Instantiate(GunChamberPrefab, new Vector3(0, 10f, 0), Quaternion.identity);
+    }
+}
