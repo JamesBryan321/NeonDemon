fa74293642f6e8c13145c88d24ae1bbb47825cf5
PapaJepo <44029409+PapaJepo@users.noreply.github.com>
2021-03-22 14:34:18 +0000

    boss damaging player

---
+++ Neon-Demon Ver.2/Assets/Boss/BossAttack.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAttack : MonoBehaviour
+{
+    [SerializeField] private GameObject Player;
+    public BoxCollider AttackZone;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AttackZone.enabled = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Player.GetComponent<PlayerHP>().PlayerHealth -= 0.25f;
+        }
+    }
+}
