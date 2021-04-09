dfe542f47a9de74b31682f2af911df3768b6039b
James Bryan <jamesbryangamess@gmail.com>
2021-04-09 13:45:45 +0100

    some changes and add ons for car park encounter

---
+++ Neon-Demon Ver.2/Assets/gateTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gateTrigger : MonoBehaviour
+{
+
+    public Animator gateanim;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            gateanim.SetTrigger("Gate");
+        }
+    }
+}
