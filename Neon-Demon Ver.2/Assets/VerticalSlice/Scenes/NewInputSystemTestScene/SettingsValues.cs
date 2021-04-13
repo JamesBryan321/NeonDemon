dde061434fbedb4870cc5206b3e77834b7da9e6c
SlimShaney <44029427+SlimShaney@users.noreply.github.com>
2021-04-13 14:57:07 +0100

    Tutorial level demon hands pushed as they are atm

---
+++ Neon-Demon Ver.2/Assets/VerticalSlice/Scenes/NewInputSystemTestScene/SettingsValues.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsValues : MonoBehaviour
+{
+    public float sensitivity;
+
+    public Slider sensitivitySlider;
+    void Start()
+    {
+        sensitivity = sensitivitySlider.value * 20;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SaveData()
+    {
+        PlayerPrefs.SetFloat("aimSensitivity", sensitivity);
+    }
+    
+    public void ChangeSensitivity()
+    {
+        sensitivity = sensitivitySlider.value * 20;
+        SaveData();
+    }
+}
