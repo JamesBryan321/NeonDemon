1bf466315afd067919b22c1ca4f7a07df52395d4
SlimShaney <44029427+SlimShaney@users.noreply.github.com>
2021-04-13 17:12:20 +0100

    PlayerPrefs Script updated and audio Mixer w/ groups created

--- Neon-Demon Ver.2/Assets/VerticalSlice/Scenes/NewInputSystemTestScene/SettingsValues.cs
+++ Neon-Demon Ver.2/Assets/VerticalSlice/Scenes/NewInputSystemTestScene/SettingsValues.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UI;
 
 public class SettingsValues : MonoBehaviour
@@ -8,6 +9,17 @@ public class SettingsValues : MonoBehaviour
     public float sensitivity;
 
     public Slider sensitivitySlider;
+    
+    public Slider masterVolumeSlider;
+    public Slider musicVolumeSlider;
+    public Slider soundFXVolumeSlider;
+    public Slider dialogueVolumeSlider;
+    
+    public AudioMixer masterMixer;
+    public AudioMixer musicMixer;
+    public AudioMixer soundFXMixer;
+    public AudioMixer dialogueMixer;
+    
     void Start()
     {
         sensitivity = sensitivitySlider.value * 20;
@@ -19,7 +31,7 @@ public class SettingsValues : MonoBehaviour
         
     }
 
-    public void SaveData()
+    public void SaveSensitivityData()
     {
         PlayerPrefs.SetFloat("aimSensitivity", sensitivity);
     }
@@ -27,6 +39,26 @@ public class SettingsValues : MonoBehaviour
     public void ChangeSensitivity()
     {
         sensitivity = sensitivitySlider.value * 20;
-        SaveData();
+        SaveSensitivityData();
+    }
+
+    public void ChangeMasterVolume()
+    {
+        masterMixer.SetFloat("MasterVolume", masterVolumeSlider.value);
+    }
+    
+    public void ChangeMusicVolume()
+    {
+        musicMixer.SetFloat("MusicVolume", musicVolumeSlider.value);
+    }
+    
+    public void ChangeSoundFXVolume()
+    {
+        soundFXMixer.SetFloat("SoundFXVolume", soundFXVolumeSlider.value);
+    }
+    
+    public void ChangeDialogueVolume()
+    {
+        dialogueMixer.SetFloat("DialogueVolume", dialogueVolumeSlider.value);
     }
 }
