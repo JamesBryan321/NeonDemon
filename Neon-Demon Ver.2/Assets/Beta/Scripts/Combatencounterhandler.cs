2ab53361f64289082dd67466f5abf519884ee232
James Bryan <jamesbryangamess@gmail.com>
2021-04-28 16:52:50 +0100

    main level changes for combat encounter

--- Neon-Demon Ver.2/Assets/Beta/Scripts/Combatencounterhandler.cs
+++ Neon-Demon Ver.2/Assets/Beta/Scripts/Combatencounterhandler.cs
@@ -9,6 +9,10 @@ public class Combatencounterhandler : MonoBehaviour
     public GameObject portal1;
     public GameObject portal2;
 
+
+    public GameObject[] enemies;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +22,23 @@ public class Combatencounterhandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        foreach (var enemy in enemies)
+        {
+            var movescript = enemy.GetComponent<MeleeEnemy>();
+
+            if(movescript == false)
+            {
+
+                off();
+            }
+
+        }
+    }
+
+    private void off()
+    {
+        portal1.SetActive(false);
+        portal2.SetActive(false);
+       
     }
 }
