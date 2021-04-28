ea4f6606e1560431b6760b2d50445c022b878b73
Dazariath <c17366881@mydit.ie>
2021-04-28 23:45:11 +0100

    implemented Gun UI

---
+++ Neon-Demon Ver.2/Assets/Gold/UI/RevolverAmmoCounter.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RevolverAmmoCounter : MonoBehaviour
+{
+    public GameObject RevolverManager;
+    public int RevolverAmmo;
+
+    public GameObject ZeroText;
+    public GameObject OneText;
+    public GameObject TwoText;
+    public GameObject ThreeText;
+    public GameObject FourText;
+    public GameObject FiveText;
+    public GameObject SixText;
+
+    public Animator RevolverBackgroundAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        Shooting revolverScript = RevolverManager.GetComponent<Shooting>();
+        RevolverAmmo = revolverScript.Ammo;
+
+        ZeroText.SetActive(false);
+        OneText.SetActive(false);
+        TwoText.SetActive(false);
+        ThreeText.SetActive(false);
+        FourText.SetActive(false);
+        FiveText.SetActive(false);
+        SixText.SetActive(false);
+
+        RevolverBackgroundAnimator = this.GetComponent<Animator>();
+    }
+
+    public void AmmoCounter()
+    {
+
+        if (RevolverAmmo == 0)
+        {
+            ZeroText.SetActive(true);
+            SixText.SetActive(false);
+        }
+        else
+        {
+            ZeroText.SetActive(false);
+        }
+
+        if (RevolverAmmo == 1)
+        {
+            OneText.SetActive(true);
+        }
+        else
+        {
+            OneText.SetActive(false);
+        }
+
+        if (RevolverAmmo == 2)
+        {
+            TwoText.SetActive(true);
+        }
+        else
+        {
+            TwoText.SetActive(false);
+        }
+
+        if (RevolverAmmo == 3)
+        {
+            ThreeText.SetActive(true);
+        }
+        else
+        {
+            ThreeText.SetActive(false);
+        }
+
+        if (RevolverAmmo == 4)
+        {
+            FourText.SetActive(true);
+        }
+        else
+        {
+            FourText.SetActive(false);
+        }
+
+        if (RevolverAmmo == 5)
+        {
+            FiveText.SetActive(true);
+        }
+        else
+        {
+            FiveText.SetActive(false);
+        }
+
+        if (RevolverAmmo == 6)
+        {
+            SixText.SetActive(true);
+            ZeroText.SetActive(false);
+        }
+        else if (RevolverAmmo < 6)
+        {
+            SixText.SetActive(false);
+        }
+    }
+
+    public void FiredShot()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            RevolverBackgroundAnimator.SetBool("ShotFired",true);
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            RevolverBackgroundAnimator.SetBool("ShotFired", false);
+        }
+    }
+
+    void Update()
+    {
+        GameObject RevolverManager = GameObject.Find("RevolverMain");
+        Shooting revolverScript = RevolverManager.GetComponent<Shooting>();
+        RevolverAmmo = revolverScript.Ammo;
+
+        AmmoCounter();
+
+        FiredShot();
+    }
+}
---
+++ Neon-Demon Ver.2/Assets/Gold/UI/Shotgun/ShotgunAmmoCounter.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShotgunAmmoCounter : MonoBehaviour
+{
+    public GameObject ShotgunManager;
+    public int ShotgunAmmo;
+
+    public GameObject ZeroText;
+    public GameObject OneText;
+    public GameObject TwoText;
+    public GameObject ThreeText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        Shooting shotgunScript = ShotgunManager.GetComponent<Shooting>();
+        ShotgunAmmo = shotgunScript.Ammo;
+
+        ZeroText.SetActive(false);
+        OneText.SetActive(false);
+        TwoText.SetActive(false);
+        ThreeText.SetActive(false);
+    }
+
+    public void AmmoCounter()
+    {
+
+        if (ShotgunAmmo == 0)
+        {
+            ZeroText.SetActive(true);
+        }
+        else
+        {
+            ZeroText.SetActive(false);
+        }
+
+        if (ShotgunAmmo == 1)
+        {
+            OneText.SetActive(true);
+        }
+        else
+        {
+            OneText.SetActive(false);
+        }
+
+        if (ShotgunAmmo == 2)
+        {
+            TwoText.SetActive(true);
+        }
+        else
+        {
+            TwoText.SetActive(false);
+        }
+
+        if (ShotgunAmmo == 3)
+        {
+            ThreeText.SetActive(true);
+        }
+        else
+        {
+            ThreeText.SetActive(false);
+        }
+
+    }
+
+    void Update()
+    {
+        Shooting shotgunScript = ShotgunManager.GetComponent<Shooting>();
+        ShotgunAmmo = shotgunScript.Ammo;
+
+        AmmoCounter();
+    }
+}
