006253038de01787dec7def2232560c7ea8afa46
EJJMC <44029567+EJJMC@users.noreply.github.com>
2021-04-30 15:04:05 +0100

    added car delete and phonebooth

---
+++ Neon-Demon Ver.2/Assets/destroycars.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroycars : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject destroy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == ("Player"))
+        {
+            Destroy(destroy);
+            Debug.Log("destroyed");
+        }
+
+    }
+}
