019aae1431cd65ad14d614ce5c92dc0cc6dc2650
PapaJepo <44029409+PapaJepo@users.noreply.github.com>
2021-05-09 18:10:53 +0100

    kill me

--- Neon-Demon Ver.2/Assets/Beta/Scripts/BraceletManager.cs
+++ Neon-Demon Ver.2/Assets/Beta/Scripts/BraceletManager.cs
@@ -14,7 +14,7 @@ public class BraceletManager : MonoBehaviour
     public GameObject braceletImage1;
     public GameObject braceletImage2;
 
-
+    public GameObject TextDelete,TextDelete2,TextDelete3;
     // Start is called before the first frame update
     void Start()
     {
@@ -29,40 +29,42 @@ public class BraceletManager : MonoBehaviour
     void Update()
     {
 
-        if(keyCount ==1 )
-        {
-            braceletImage1.SetActive(true);
-        }
-        if (keyCount == 2)
-        {
-            goToNC.SetActive(true);
-            braceletImage2.SetActive(true);
-
-        }
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Key"))
         {
-            keyCount++;
+            keyCount += 1;
             Destroy(other.gameObject);
+
+            if (keyCount == 1)
+            {
+                braceletImage1.SetActive(true);
+            }
+            if (keyCount == 2)
+            {
+                goToNC.SetActive(true);
+                braceletImage2.SetActive(true);
+                Destroy(TextDelete);
+                Destroy(TextDelete2);
+                Destroy(TextDelete3);
+            }
         }
-        if (other.gameObject.CompareTag("Nightclub")&& keyCount>=2)
+        if (other.gameObject.CompareTag("Nightclub") && keyCount >= 2)
         {
             Time.timeScale = 1f;
             SceneManager.LoadScene("Boss");
         }
         if (other.gameObject.CompareTag("Nightclub") && keyCount <= 2)
         {
-         //   goToNC1.SetActive(true);
+            //   goToNC1.SetActive(true);
         }
     }
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.CompareTag("Nightclub") && keyCount <= 2)
         {
-          //  goToNC1.SetActive(false);
+            //  goToNC1.SetActive(false);
         }
     }
-
 }
